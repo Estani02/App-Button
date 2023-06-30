673fd0938b59680c9227e2cdeb7f101b2a0f3c29
Estani02 <estaniolmedo@gmail.com>
2023-06-30 18:09:00 -0300

    component rendering

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import Button from "./components/Button"
+import api from './api'
+import { DataButton } from './interfaces'
+import { CreationForm } from './components/CreationForm'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<DataButton[]>([])
+
+  useEffect(() => {
+    api.get('/buttons').then((res) => {
+      setData(res.data)
+    })
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='h-screen flex flex-col items-center mt-12 gap-28'>
+      <header>
+        <h1 className='text-3xl underline'>CONTROL DE BOTÓNES</h1>
+      </header>
+      <CreationForm onDataUpdate={setData} />
+      <Button onDataUpdate={setData} data={data} />
+    </div>
   )
 }
 
