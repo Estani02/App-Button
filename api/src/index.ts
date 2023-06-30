7c4c146cd3f775e57185fe54835c3a74cabf7936
Estani02 <estaniolmedo@gmail.com>
2023-06-29 23:34:16 -0300

    connection to the database

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,15 @@
 import app from './app'
+import sequelize from './db'
+import './models/buttons'
 
 const PORT = process.env.PORT ?? 3000
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+void sequelize.sync({ force: false }).then(() => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`)
+    })
+  } catch (error) {
+    console.error('❌ Error starting server:', error)
+  }
 })
